a47134e0ff714d290d997ca41852296ec86bf382
nathan.bonilla <nbonilla409@gmail.com>
2022-09-11 00:24:26 -0700

    modify yaml

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'renderTest.apps.RendertestConfig',
+    'renderTest.apps.RendertestConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
